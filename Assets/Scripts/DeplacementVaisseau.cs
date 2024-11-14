b957a8b0a853b1ffabcdcb86585985cd101ff157
safaa02 <122892364+safaa02@users.noreply.github.com>
2024-11-14 20:54:59 +0100

    Mouvement du Player avec les inputs (rotation et position)

---
+++ Assets/Scripts/DeplacementVaisseau.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DeplacementVaisseau : MonoBehaviour
+{
+    //[SerializeField] private float vitesseDeplacement;
+    [SerializeField] private float vitesseRotation = 150f;
+    [SerializeField] private float vitessePosition = 5f;
+
+    
+    void Start(){   }
+
+
+    void Update()
+    {
+        // Déplacer le Game Objet personnage 
+
+        //Position
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 positionMouvement = new Vector3(0, verticalInput, 0) * vitessePosition * Time.deltaTime;
+        transform.Translate(positionMouvement);
+
+        //Rotation
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float rotation = horizontalInput * vitesseRotation * Time.deltaTime;        
+        transform.Rotate(0, 0, -rotation);
+
+        //Message Log
+        Debug.Log("Déplacement du vaisseau !!");
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ennemi"))
+        {
+            Debug.Log("Une collision !!!!!");
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -7,14 +7,13 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField]
     private GameObject missile;
 
-    // Update is called once per frame
+
     void Update()
-    {
-        
+    {        
         if(Input.GetMouseButtonDown(0))
         {
             Instantiate(missile, transform.position, Quaternion.identity);
-        }
+        }        
     }
 
 
