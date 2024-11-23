95b9051d10f0ff10fff52253bbd90d9cb6d97bc7
safaa02 <122892364+safaa02@users.noreply.github.com>
2024-11-24 00:06:25 +0100

    script player (1.2)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,30 +7,26 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     //Deplacement
-    [SerializeField] private float vitesseRot;
-    [SerializeField] private float vitessePos;
-
-    //Vie
-    [SerializeField] private float vie;
-    [SerializeField] private float degatEnnemi;
-
-    // ------- TO DO -------
-    [SerializeField] private string sceneCharger;
+    [SerializeField] private float vitesseRotation = 150f;
+    [SerializeField] private float vitessePosition = 5f;
 
     //Projectile lancement
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject prefabProjectile;
+    [SerializeField] private GameObject positionLancementProjectile;
     [SerializeField] private float forceProjectile = 1000f;
     [SerializeField] private float tempsDeVie = 2f;
 
-    [SerializeField] private GameObject projectilePos;
+    //Vie
+    [SerializeField] private float pointsDeVie= 100f;
+    [SerializeField] private float degatParEnnemi = 25f;
+    
+    // ------- TO DO -------
+    [SerializeField] private string sceneCharge;
+    
 
     void Start()
     {
-        vitesseRot = 150f;
-        vitessePos = 5f;
 
-        vie = 100f;
-        degatEnnemi = 25f;
     }
 
     void Update()
@@ -44,17 +40,41 @@ public class Player : MonoBehaviour
         gestionVie(collision);
     }
 
+
     private void deplacement()
     {
         //Position
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 positionMouvement = new Vector3(0, verticalInput, 0) * vitessePos * Time.deltaTime;
+        Vector3 positionMouvement = new Vector3(0, verticalInput, 0) * vitessePosition * Time.deltaTime;
         this.transform.Translate(positionMouvement);
 
         //Rotation
         float horizontalInput = Input.GetAxis("Horizontal");
-        float rotation = horizontalInput * vitesseRot * Time.deltaTime;
+        float rotation = horizontalInput * vitesseRotation * Time.deltaTime;
         this.transform.Rotate(0, 0, -rotation);
+    }
+
+    private void lancerProjectile()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //Instanciation du projectile
+            GameObject projectile = Instantiate(prefabProjectile, positionLancementProjectile.transform.position, Quaternion.identity) as GameObject;
+
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                rb.AddForce(positionLancementProjectile.transform.up * forceProjectile);
+            }
+            else
+            {
+                Debug.LogError("Le Rigidbody2D est manquant sur le projectile !");
+            }
+
+            //Détruire le projectile après 2 secondes
+            Destroy(projectile, tempsDeVie);
+        }
     }
 
     private void gestionVie(Collision2D collision)
@@ -66,10 +86,10 @@ public class Player : MonoBehaviour
                 Destroy(collision.gameObject);
             }
 
-            vie = vie - degatEnnemi;
-            Debug.Log("La vie du player est à " + vie);
+            pointsDeVie= pointsDeVie- degatParEnnemi;
+            Debug.Log("La vie du player est à " + pointsDeVie);
 
-            if (vie <= 0)
+            if (pointsDeVie<= 0)
             {
                 Destroy(this.gameObject);
                 Debug.Log("Le player est détruit !");
@@ -85,30 +105,7 @@ public class Player : MonoBehaviour
     // ------- TO DO -------
     private void gestionScene()
     {
-        SceneManager.LoadScene(sceneCharger);
-    }
-
-    private void lancerProjectile()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            //Instanciation du projectile
-            GameObject projectile = Instantiate(projectilePrefab, projectilePos.transform.position, Quaternion.identity) as GameObject;
-
-            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-
-            if (rb != null)
-            {
-                rb.AddForce(projectilePos.transform.up * forceProjectile);
-            }
-            else
-            {
-                Debug.LogError("Le Rigidbody2D est manquant sur le projectile !");
-            }
-
-            //Détruire le projectile après 2 secondes
-            Destroy(projectile, tempsDeVie);
-        }
+        SceneManager.LoadScene(sceneCharge);
     }
 
 }
