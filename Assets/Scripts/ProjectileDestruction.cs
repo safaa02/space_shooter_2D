7147de86b392f69cc8fb26eb7a8ebbc914cd6509
safaa02 <122892364+safaa02@users.noreply.github.com>
2024-11-24 01:17:20 +0100

    changement script destruction

--- Assets/Scripts/ProjectileDestruction.cs
+++ Assets/Scripts/ProjectileDestruction.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileDetruit : MonoBehaviour
+public class ProjectileDestruction : MonoBehaviour
 {
     void Start(){ }
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestructionSurMur(collision);
+    }
+
+
+    private void DestructionSurMur(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Mur"))
         {
             Destroy(this.gameObject);
         }
     }
-
 }
